4ef595f3d9816c3ced011c2ab0adf3d979aaa3d9
Kevin Paul Montealegre Melo <kmontealegremelo@gmail.com>
2020-09-15 19:36:44 -0500

    calculations time and date class and methods

--- app/src/main/java/com/example/habittrackermvvm/utils/Calculations.kt
+++ app/src/main/java/com/example/habittrackermvvm/utils/Calculations.kt
@@ -28,7 +28,33 @@ object Calculations {
         val hours = difference / 60 / 1000 / 60
         val days = (difference / 60 / 1000 / 60 ) / 24
         val months = ((difference / 60 / 1000 / 60 ) / 24 / (365/12))
+        val years = difference / 60 / 1000 / 60  / 24 / 365
 
+        if (isNegative) {
+            return when {
+                minutes < 240 -> "Start in $minutes minutes"
+
+                hours < 48 -> "Start in $hours hours"
+
+                days < 61 -> "Starts in $days days"
+
+                months < 24 -> "Starts in $months months"
+
+                else -> "Starts in $years years"
+            }
+        }
+
+        return when {
+            minutes < 240 -> "Start in $minutes minutes"
+
+            hours < 48 -> "Start in $hours hours"
+
+            days < 61 -> "Starts in $days days"
+
+            months < 24 -> "Starts in $months months"
+
+            else -> "Starts in $years years"
+        }
 
     }
     
@@ -39,6 +65,36 @@ object Calculations {
         val date = sdf.format((Date(stamp.time)))
         
         return date.toString()
+    }
+
+   fun cleanDate (_day: Int, _month:Int, _year:Int) : String {
+       var day = _day.toString()
+       var month = _month.toString()
+       var year = _year.toString()
+
+       if (_day < 10) {
+           day = "0 $day"
+       }
+
+       if (_month < 9) {
+           month = "0 ${_month + 1}"
+       }
+       return "$day/$month/$_year"
+    }
+
+    fun cleanTime(_hour:Int, _minute: Int, _sec:Int) : String {
+        var hour = _hour.toString()
+        var minute = _minute.toString()
+
+        if(_hour < 10) {
+            hour = "0$_hour"
+        }
+
+        if(_minute < 10) {
+            hour = "0$_minute"
+        }
+
+        return "$hour:$minute"
     }
     
     
